61328549c1f3610abae656704a6e675e15f320f7
arun <aruns@sw git config --global user.email aruns@swiftsetup.comiftsetup.com>
2014-12-30 13:20:59 +0530

    deployed on heroku

--- server/config/config.js
+++ server/config/config.js
@@ -2,7 +2,7 @@ var path = require('path');
 var rootPath = path.normalize(__dirname +'/../../');
 module.exports = {
     development: {
-        db: 'mongodb://localhost/activities',
+        db: 'mongodb://sahni:sahni@ds029831.mongolab.com:29831/courses',
         rootPath: rootPath,
         port: process.env.PORT || 3030
     },
